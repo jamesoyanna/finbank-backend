c14de48628d76f59d9d75de2619e713b0a5f0851
james <engrjayt200@gmail.com>
2021-10-27 23:11:03 +0100

    Bug fixes

--- finbank-backend/config/db.js
+++ finbank-backend/config/db.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-   await mongoose.connect(
-     "mongodb+srv://james:reactnodeapp@cluster0.3ucmj.mongodb.net/ReactNodeDB?retryWrites=true&w=majority",
-     {
+   await mongoose.connect(process.env.MONGO_URI, {
        useNewUrlParser: true,
        useCreateIndex: true,
        useUnifiedTopology: true,
-       useFindAndModify: true,
-     }
-   );
+       useFindAndModify: true
+   });
 
    console.log("MongoDB connected successully")
 }
