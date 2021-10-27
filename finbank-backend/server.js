6d9b8bc4fc94aa1c632d37c18b897cb89b3dff48
james <engrjayt200@gmail.com>
2021-10-27 22:05:39 +0100

    Add config file

--- finbank-backend/server.js
+++ finbank-backend/server.js
@@ -19,6 +19,12 @@ app.use("/api/private", require("./routes/private"));
 // Error Handler Middleware
 app.use(errorHandler);
 
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () =>
